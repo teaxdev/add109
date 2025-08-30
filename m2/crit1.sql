e27b8848b9cd9c35eb6980bbd822eb0e0a9bf2df
teaxdev <FloresAlex6904@gmail.com>
2025-08-30 14:45:43 -0500

    finish crit1

--- m2/crit1.sql
+++ m2/crit1.sql
@@ -1,9 +1,9 @@
+-- I will list countries by name, add 'Petoria', update France, and lastly delete Petoria.
 SELECT * FROM Country ORDER BY Name;
 
 INSERT INTO Country (Name, Code, LifeExpectancy, Continent)
     VALUES ('Petoria', 'PTR', '1000', 'North America');
 -- 169
-UPDATE Country SET Name = 'French Republic';
+UPDATE Country SET Name = 'French Republic' WHERE Code = 'FRA';
 -- 74
 DELETE FROM Country WHERE Code = 'PTR';
-SELECT * FROM Country;
